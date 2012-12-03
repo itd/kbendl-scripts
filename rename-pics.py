5bebf8705aba162d7b14ef6036f4d3d9cc3a1766
Kurt Bendl <kurt@tool.net>
2012-12-03 09:47:47 -0700

    Doc tweak.

--- rename-pics.py
+++ rename-pics.py
@@ -7,6 +7,10 @@ directory that are in the format:
     cam1-12-11-29_15-26-32-22.jpg
 and renames them to:
     12112915263222.jpg
+
+I used this to rename the pics captured from
+AXIS cameras by the app
+    https://github.com/itd/axispics/
 """
 import os
 
